6c0a0c7d7a19445d05f1b05efef481871ad567ac
yudaikobayashi <yudaiiaduy5296@gmail.com>
2020-08-30 03:40:14 +0900

    Removed click count on canvas

--- app/src/main/java/com/example/simplecounter/MainActivity.java
+++ app/src/main/java/com/example/simplecounter/MainActivity.java
@@ -16,19 +16,23 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         findViewById(R.id.minus).setOnClickListener(this);
         findViewById(R.id.plus).setOnClickListener(this);
         findViewById(R.id.layout).setOnClickListener(this);
-
     }
 
     @Override
     public void onClick(View view) {
+        if (((TextView) findViewById(R.id.count)).getText().toString().equals("")) {
+            ((TextView) findViewById(R.id.count)).setText("0");
+        }
         TextView count = findViewById(R.id.count);
-        getCurrentFocus().clearFocus();
+        //getCurrentFocus().clearFocus();
+        findViewById(R.id.title).clearFocus();
+        findViewById(R.id.count).clearFocus();
         switch (view.getId()) {
             case R.id.minus:
                 count.setText(String.valueOf(Integer.parseInt(count.getText().toString()) - 1));
                 break;
             case R.id.plus:
-                //case R.id.layout:
+            case R.id.layout:
                 count.setText(String.valueOf(Integer.parseInt(count.getText().toString()) + 1));
                 break;
             default:
