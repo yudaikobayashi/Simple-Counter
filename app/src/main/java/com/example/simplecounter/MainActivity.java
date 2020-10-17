7928b6828f88908530740712875b0650c5e195ef
yudaikobayashi <yudaiiaduy5296@gmail.com>
2020-10-18 07:52:56 +0900

    Save input values as SharedPreferences.

--- app/src/main/java/com/example/simplecounter/MainActivity.java
+++ app/src/main/java/com/example/simplecounter/MainActivity.java
@@ -1,13 +1,18 @@
 package com.example.simplecounter;
 
 import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.EditText;
-import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.preference.PreferenceManager;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
@@ -16,10 +21,25 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        if (savedInstanceState != null) {
-            ((EditText) findViewById(R.id.title)).setText(savedInstanceState.getString("title"));
-            ((EditText) findViewById(R.id.count)).setText(savedInstanceState.getString("count"));
+        SharedPreferences.Editor editor = this.getPreferences(MODE_PRIVATE).edit();
+        if (PreferenceManager.getDefaultSharedPreferences(this)
+                .getBoolean("save_title", false)) {
+            String title = this.getPreferences(MODE_PRIVATE).getString("title", "");
+            assert title != null;
+            if (!title.equals("")) {
+                ((EditText) findViewById(R.id.title)).setText(title);
+            }
+        } else if (savedInstanceState == null) {
+            editor.putString("title", "");
         }
+        if (PreferenceManager.getDefaultSharedPreferences(this)
+                .getBoolean("save_count", false)) {
+            int count = this.getPreferences(MODE_PRIVATE).getInt("count", 0);
+            ((EditText) findViewById(R.id.count)).setText(String.valueOf(count));
+        } else if (savedInstanceState == null) {
+            editor.putInt("count", 0);
+        }
+        editor.apply();
 
         findViewById(R.id.minus).setOnClickListener(this);
         findViewById(R.id.plus).setOnClickListener(this);
@@ -27,14 +47,24 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     }
 
     @Override
-    protected void onSaveInstanceState(Bundle outState) {
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu, menu);
+        return true;
+    }
+
+
+    /*@Override
+    protected void onSaveInstanceState(@NonNull Bundle outState) {
         super.onSaveInstanceState(outState);
         outState.putString("title", ((EditText) findViewById(R.id.title)).getText().toString());
         outState.putString("count", ((EditText) findViewById(R.id.count)).getText().toString());
-    }
+    }*/
 
     @Override
     public void onClick(View view) {
+        SharedPreferences.Editor editor = this.getPreferences(MODE_PRIVATE).edit();
+        EditText count = findViewById(R.id.count);
         View viewFocused = this.getCurrentFocus();
         if (viewFocused != null) {
             viewFocused.clearFocus();
@@ -42,20 +72,48 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                     = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
             manager.hideSoftInputFromWindow(viewFocused.getWindowToken(), 0);
         }
-        if (((TextView) findViewById(R.id.count)).getText().toString().equals("")) {
-            ((TextView) findViewById(R.id.count)).setText("0");
+        editor.putString("title", String.valueOf(((EditText) findViewById(R.id.title)).getText()));
+        editor.apply();
+        if (count.getText().toString().equals("")) {
+            count.setText("0");
+            editor.putInt("count", 0);
+            editor.apply();
         }
-        TextView count = findViewById(R.id.count);
         switch (view.getId()) {
             case R.id.minus:
                 count.setText(String.valueOf(Integer.parseInt(count.getText().toString()) - 1));
+                editor.putInt("count", Integer.parseInt(count.getText().toString()));
+                editor.apply();
                 break;
             case R.id.plus:
-            case R.id.layout:
+                //case R.id.layout:
                 count.setText(String.valueOf(Integer.parseInt(count.getText().toString()) + 1));
+                editor.putInt("count", Integer.parseInt(count.getText().toString()));
+                editor.apply();
                 break;
             default:
-                break;
         }
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        SharedPreferences.Editor editor = this.getPreferences(MODE_PRIVATE).edit();
+        switch (item.getItemId()) {
+            case R.id.reset_title:
+                ((EditText) findViewById(R.id.title)).setText("");
+                editor.putString("title", "");
+                break;
+            case R.id.reset_count:
+                editor.putInt("count", 0);
+                ((EditText) findViewById(R.id.count)).setText("0");
+                break;
+            case R.id.action_settings:
+                Intent intent = new Intent(this, SettingsActivity.class);
+                startActivity(intent);
+                break;
+            default:
+        }
+        editor.apply();
+        return super.onOptionsItemSelected(item);
     }
 }
